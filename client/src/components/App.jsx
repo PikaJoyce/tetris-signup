4743c72ca4985b49adf6c48140f8455dc4c474d2
Joyce Ma <pikajoyce@gmail.com>
2019-10-22 17:42:41 -0700

    Refactored to using a module since my custom hook wasn't working. Works now and connected it to express server.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import SignUp from './SignUp.jsx';
+import useForm from 'react-hook-form';
+import axios from 'axios';
 
-const App = () => (
-  <div>
-    <SignUp />
-  </div>
-);
+export default function App() {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => {
+    console.log('dis is data', data)
+    axios.post('/signUp', data)
+      .then(res => {
+        console.log("we did it", res)
+      })
+      .catch(err => {
+        console.log("u fked up", err)
+      })
+  };
+  console.log(errors);
 
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="First name" name="First name" ref={register({ required: true, maxLength: 80 })} />
+      <input type="text" placeholder="Last name" name="Last name" ref={register({ required: true, maxLength: 100 })} />
+      <input type="text" placeholder="Email" name="Email" ref={register({ required: true, pattern: /^\S+@\S+$/i })} />
+      <input type="tel" placeholder="Mobile number" name="Mobile number" ref={register({ required: true, minLength: 6, maxLength: 12 })} />
+      <input type="text" placeholder="Discord" name="Discord" ref={register({ required: true })} />
 
-export default App;
+      <input type="submit" />
+    </form>
+  );
+}
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 // Import the path module from node to create absolute file paths for express static
 const path = require('path');
 
+const bodyParser = require('body-parser')
+
 // Instantiate the express server
 const app = express();
 // Set a constant for the port that our express server will listen on
@@ -10,19 +12,14 @@ const PORT = 3000;
 
 // Serve static files. Any requests for specific files will be served if they exist in the provided folder
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.use(bodyParser.json());
+
 // Start the server on the provided port
 app.listen(PORT, () => console.log('Listening on port: ' + PORT));
 
 
-// app.post('/*', (req, res) => {
-
-// })
-
-// app.get('*', async (req, res) => {
-//   try {
-
-//   }
-//   catch{
-
-//   }
-// })
+app.post('/signUp', (req, res) => {
+  const { body } = req
+  console.log('the bod', body)
+})
